7a42a5323a186cffc76440c1aa8ecec038bf003c
xiechao06 <xiechao06@gmail.com>
2013-10-25 18:18:52 +0800

    temporary

--- litefac_launchpad/basemain.py
+++ litefac_launchpad/basemain.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("litefac_launchpad.default_settings")
 
 
 @app.route('/')
 def index():
-    return 'hello, world'
+    return render_template('index.html')
